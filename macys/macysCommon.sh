974b65d99de5740bd9800673d7bb7d8ca06557fd
eugenp <hanriseldon@gmail.com>
2012-03-20 14:08:56 +0200

    upgraded solr to work with latest tomcat and solr

--- macys/macysCommon.sh
+++ macys/macysCommon.sh
@@ -7,15 +7,7 @@ source ../devel/devel_common.sh
 MACYS_BASEDIR=$SCRIPTS_BASEDIR/macys
 WORKSPACE=/opt/eclipse/workspaces/Eclipse_old/stella3_current_branch/stella-ui
 
-SOLR_NAME=apache-solr-3.4.0
+SOLR_LOCATION=/opt/solr/runtime
+SOLR_NAME=apache-solr-3.5.0
 SOLR_ARCHIVE=$SOLR_NAME.zip
-SOLR_TOMCAT=$TOMCAT_BASEDIR/apache-tomcat-7.0.22_SOLR
-
-# JBOSS_KIT_DIR=/opt/kits/jboss
-# JBOSS_DIR=/opt/jboss/
-# JBOSS_NAME=jboss-4.2.3.GA
-# JBOSS_ARCHIVE=$JBOSS_NAME.zip
-# JBOSS_INSTANCE=$JBOSS_DIR/$JBOSS_NAME
-
-# MACYS_TOMCAT=$TOMCAT_BASEDIR/apache-tomcat-7.0.22_MACYS
-
+SOLR_TOMCAT=$TOMCAT_BASEDIR/apache-tomcat-7.0.26_SOLR
